b82c0120380bdd89b6bc262a8f1cee75f6d519e8
Samalona <caniagooo@gmail.com>
2025-05-31 16:31:49 +0700

    fixing attendance & skoring

---
+++ storage/framework/views/b333b764c036d9a69697c253677b2bc4.php
@@ -0,0 +1,49 @@
+<?php if (isset($component)) { $__componentOriginal1c2e2f4f77e507b499e79defc0d48b7e = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal1c2e2f4f77e507b499e79defc0d48b7e = $attributes; } ?>
+<?php $component = App\View\Components\DefaultLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('default-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\App\View\Components\DefaultLayout::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+    <div class="container mt-5">
+        <h1>Attendance for Session on <?php echo e($session->session_date); ?></h1>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Participant</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $__currentLoopData = $attendances; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $attendance): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <tr>
+                        <td><?php echo e($loop->iteration); ?></td>
+                        <td><?php echo e($attendance->student->user->name ?? $attendance->trainer->user->name); ?></td>
+                        <td><?php echo e(ucfirst($attendance->status)); ?></td>
+                        <td>
+                            <form action="<?php echo e(route('attendances.destroy', [$session->id, $attendance->id])); ?>" method="POST" style="display:inline;">
+                                <?php echo csrf_field(); ?>
+                                <?php echo method_field('DELETE'); ?>
+                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </tbody>
+        </table>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal1c2e2f4f77e507b499e79defc0d48b7e)): ?>
+<?php $attributes = $__attributesOriginal1c2e2f4f77e507b499e79defc0d48b7e; ?>
+<?php unset($__attributesOriginal1c2e2f4f77e507b499e79defc0d48b7e); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal1c2e2f4f77e507b499e79defc0d48b7e)): ?>
+<?php $component = $__componentOriginal1c2e2f4f77e507b499e79defc0d48b7e; ?>
+<?php unset($__componentOriginal1c2e2f4f77e507b499e79defc0d48b7e); ?>
+<?php endif; ?><?php /**PATH C:\Users\JITU\swim\resources\views/attendances/index.blade.php ENDPATH**/ ?>
